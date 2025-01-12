adc2344ffc5da0976ffcd81e6bcfec97dddd475d
SukeshSrinivas <123338848+SukeshSrinivas@users.noreply.github.com>
2025-01-12 11:38:41 -0600

    Done

--- Problem 5 Customers with strictly Increasing Purchases.sql
+++ Problem 5 Customers with strictly Increasing Purchases.sql
@@ -4,10 +4,11 @@ WITH CTE AS (
     FROM orders
     GROUP BY customer_id, Year
 )
-SELECT DISTINCT c1.customer_id
+SELECT c1.customer_id
 FROM CTE c1
 LEFT JOIN CTE c2
 ON c1.customer_id = c2.customer_id
    AND c1.Year + 1 = c2.Year
    AND c1.TotalPurchase < c2.TotalPurchase
-WHERE c2.customer_id IS NOT NULL;
+GROUP BY c1.customer_id
+HAVING (COUNT(*)- COUNT(c2.customer_id))=1
